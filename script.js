251d9d533ea1208eb603929d5a6395be56c55e7d
davidg7188 <114589891+davidg7188@users.noreply.github.com>
2022-09-28 09:31:00 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "David Gandara"; // update the part BETWEEN the "quotes"
+let userName = "davidg7188"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
